35e770fdf5c25589693f0826938011c7b0723442
ryian88 <95901373+ryian88@users.noreply.github.com>
2025-11-03 17:59:57 +0900

    Add files via upload

---
+++ js/quizData.js
@@ -0,0 +1,102 @@
+export const quizData = [
+  {
+    question: "다음 중 땅 속에서 생활하며 시력이 거의 없는 포유류를 고르세요.",
+    options: ["두더지", "사슴", "낙타"],
+    answer: 0,
+  },
+  {
+    question: "다음 중 포유류가 <span class='underline'>아닌</span> 동물을 고르세요.",
+    options: ["고양이", "개", "참새"],
+    answer: 2,
+  },
+  {
+    question: "다음 중 심해에서 빛을 내는 생물을 고르세요.",
+    options: ["심해오징어", "거북이", "펭귄"],
+    answer: 0,
+  },
+  {
+    question: "다음 중 해수와 민물을 모두 생활할 수 <span class='underline'>없는</span> 어류를 고르세요.",
+    options: ["장어", "연어", "금붕어"],
+    answer: 2,
+  },
+  {
+    question: "다음 중 초식동물이 <span class='underline'>아닌</span> 것을 고르세요.",
+    options: ["토끼", "사자", "기린"],
+    answer: 1,
+  },
+  {
+    question: "다음 중 가장 빠른 동물을 고르세요.",
+    options: ["치타", "토끼", "코끼리"],
+    answer: 0,
+  },
+  {
+    question: "다음 중 꼬리가 고리 모양이고 줄무늬가 있는 동물을 고르세요.",
+    options: ["라쿤", "너구리", "오소리"],
+    answer: 0,
+  },
+  {
+    question: "다음 중 알을 낳는 동물을 고르세요.",
+    options: ["호랑이", "닭", "고양이"],
+    answer: 1,
+  },
+  {
+    question: "다음 중 양서류를 고르세요.",
+    options: ["개구리", "뱀", "독수리"],
+    answer: 0,
+  },
+  {
+    question: "다음 중 겨울잠을 자는 동물을 고르세요.",
+    options: ["곰", "말", "닭"],
+    answer: 0,
+  },
+  {
+    question: "다음 중 바다에 사는 동물을 고르세요.",
+    options: ["상어", "사자", "늑대"],
+    answer: 0,
+  },
+  {
+    question: "다음 중 꼬리가 없는 동물을 고르세요.",
+    options: ["원숭이", "사람", "고양이"],
+    answer: 1,
+  },
+  {
+    question: "다음 중 풀을 먹는 동물을 고르세요.",
+    options: ["호랑이", "사슴", "독수리"],
+    answer: 1,
+  },
+  {
+    question: "다음 중 초음파를 사용해 먹이를 찾는 동물을 고르세요.",
+    options: ["박쥐", "고양이", "오리너구리"],
+    answer: 0,
+  },
+  {
+    question: "다음 중 곤충이 <span class='underline'>아닌</span> 동물을 고르세요.",
+    options: ["개미", "잠자리", "거미"],
+    answer: 2,
+  },
+  {
+    question: "다음 중 밤에 주로 활동하는 동물을 고르세요.",
+    options: ["부엉이", "닭", "다람쥐"],
+    answer: 0,
+  },
+  {
+    question: "다음 중 초식동물을 고르세요.",
+    options: ["사슴", "호랑이", "늑대"],
+    answer: 0,
+  },
+  {
+    question: "다음 중 포유류이면서 알을 낳는 동물을 고르세요.",
+    options: ["고양이", "오리너구리", "사자"],
+    answer: 1,
+  },
+  {
+    question: "다음 중 사막에서 잘 적응하여 사는 동물을 고르세요.",
+    options: ["낙타", "펭귄", "곰"],
+    answer: 0,
+  },
+  {
+    question: "다음 중 나무 위에서 생활하는 동물을 고르세요.",
+    options: ["코알라", "코끼리", "물개"],
+    answer: 0,
+  },
+];
